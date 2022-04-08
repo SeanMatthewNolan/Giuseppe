bd2c9ac39738ea9394ba43bd497df9192b8337d5
Sean Matthew Nolan <Sean.Matthew.Nolan@gmail.com>
2022-04-08 12:04:44 -0500

    Added Zermelo problem with current variation

---
+++ examples/ocp/zermelo/zermelo_current_variation.py
@@ -0,0 +1,52 @@
+import pickle
+
+import giuseppe
+
+zermelo = giuseppe.io.InputOCP()
+
+zermelo.set_independent('t')
+
+zermelo.add_state('x', 'v*cos(theta) + c*y')
+zermelo.add_state('y', 'v*sin(theta)')
+
+zermelo.add_control('theta')
+
+zermelo.add_constant('v', 1)
+zermelo.add_constant('c', -1)
+
+zermelo.add_constant('x_0', 3.5)
+zermelo.add_constant('y_0', -1.8)
+
+zermelo.add_constant('x_f', 0.)
+zermelo.add_constant('y_f', 0.)
+
+zermelo.set_cost('0', '0', 't')
+
+zermelo.add_constraint('initial', 't')
+zermelo.add_constraint('initial', 'x - x_0')
+zermelo.add_constraint('initial', 'y - y_0')
+
+zermelo.add_constraint('terminal', 'x - x_f')
+zermelo.add_constraint('terminal', 'y - y_f')
+
+with giuseppe.utils.Timer(prefix='Complilation Time:'):
+    sym_ocp = giuseppe.problems.SymOCP(zermelo)
+    sym_dual = giuseppe.problems.SymDual(sym_ocp)
+    sym_bvp = giuseppe.problems.SymDualOCP(sym_ocp, sym_dual, control_method='algebraic')
+    comp_dual_ocp = giuseppe.problems.CompDualOCP(sym_bvp, use_jit_compile=False)
+    num_solver = giuseppe.numeric_solvers.ScipySolveBVP(comp_dual_ocp, use_jit_compile=False)
+
+guess = giuseppe.guess_generators.generate_constant_guess(comp_dual_ocp)
+seed_sol = num_solver.solve(guess.k, guess)
+sol_set = giuseppe.continuation.SolutionSet(sym_bvp, seed_sol)
+cont = giuseppe.continuation.ContinuationHandler(sol_set)
+cont.add_linear_series(6, {'c': 1})
+
+with giuseppe.utils.Timer(prefix='Continuation Time:'):
+    for series in cont.continuation_series:
+        for k, last_sol in series:
+            sol_i = num_solver.solve(k, last_sol)
+            sol_set.append(sol_i)
+
+with open('current_variation_sol_set.data', 'wb') as file:
+    pickle.dump(sol_set, file)
---
+++ examples/ocp/zermelo/zermelo_current_variation_plot.py
@@ -0,0 +1,39 @@
+import pickle
+
+import matplotlib.pyplot as plt
+
+with open('current_variation_sol_set.data', 'rb') as file:
+    sol_set = pickle.load(file)
+
+fig = plt.figure()
+fig.suptitle('Zermelo Current Variation')
+
+ax11 = fig.add_subplot(2, 1, 1)
+ax11.set_xlabel(r'$x$')
+ax11.set_ylabel(r'$y$')
+
+ax12 = fig.add_subplot(2, 1, 2)
+ax12.set_xlabel(r'$t$')
+ax12.set_ylabel(r'$\theta$')
+
+fig_lam = plt.figure()
+fig_lam.suptitle('Costates')
+
+ax21 = fig_lam.add_subplot(2, 1, 1)
+ax21.set_xlabel(r'$t$')
+ax21.set_ylabel(r'$\lambda_x$')
+
+ax22 = fig_lam.add_subplot(2, 1, 2)
+ax22.set_xlabel(r'$t$')
+ax22.set_ylabel(r'$\lambda_y$')
+
+for sol in sol_set:
+    ax11.plot(sol.x[0, :], sol.x[1, :])
+    ax12.plot(sol.t, sol.u[0, :])
+
+    ax21.plot(sol.t, sol.lam[0, :])
+    ax22.plot(sol.t, sol.lam[1, :])
+
+fig_lam.tight_layout()
+
+plt.show()
