a4532bf546810199d3becaababf187a674368fcb
winstonlevin <wlevin@purdue.edu>
2022-04-19 15:21:56 -0400

    add Adiff implementation for BVP

--- giuseppe/problems/bvp/adiff.py
+++ giuseppe/problems/bvp/adiff.py
@@ -1,41 +1,58 @@
 from copy import deepcopy
 from typing import Union
+from warnings import warn
 
-from giuseppe.utils.complilation import lambdify
+import casadi as ca
+
 from giuseppe.utils.mixins import Picky
 from giuseppe.utils.typing import NumbaFloat, NumbaArray
 from .symbolic import SymBVP
-from ..components.compiled import CompBoundaryConditions
+from .compiled import CompBVP
+from ..components.adiff import AdiffBoundaryConditions, ca_wrap
 
 
-class CompBVP(Picky):
-    SUPPORTED_INPUTS: type = Union[SymBVP]
+class AdiffBVP(Picky):
+    SUPPORTED_INPUTS: type = Union[SymBVP, CompBVP]
 
     def __init__(self, source_bvp: SUPPORTED_INPUTS, use_jit_compile=True):
         Picky.__init__(self, source_bvp)
 
-        self.use_jit_compile = use_jit_compile
-        self.src_bvp = deepcopy(source_bvp)  # source dual_ocp is copied here for reference as it may be mutated later
+        self.src_bvp = deepcopy(source_bvp)
 
-        self.num_states = len(self.src_bvp.states)
-        self.num_parameters = len(self.src_bvp.parameters)
-        self.num_constants = len(self.src_bvp.constants)
-        self.default_values = self.src_bvp.default_values
+        if isinstance(self.src_bvp, CompBVP):
+            if self.src_bvp.use_jit_compile:
+                warn('AdiffDual cannot accept JIT compiled CompDual! Recompiling CompDual without JIT...')
+                self.comp_bvp: CompBVP = CompBVP(self.src_bvp.src_bvp, use_jit_compile=False)
+            else:
+                self.comp_bvp: CompBVP = deepcopy(self.src_bvp)
+        else:
+            self.comp_bvp: CompBVP = CompBVP(self.src_bvp, use_jit_compile=False)
 
-        self.sym_args = (self.src_bvp.independent, self.src_bvp.states.flat(), self.src_bvp.parameters.flat(),
-                         self.src_bvp.constants.flat())
-        self.args_numba_signature = (NumbaFloat, NumbaArray, NumbaArray, NumbaArray)
+        self.num_states = self.comp_bvp.num_states
+        self.num_parameters = self.comp_bvp.num_parameters
+        self.num_constants = self.comp_bvp.num_constants
+        self.default_values = self.comp_bvp.default_values
 
-        self.dynamics = self.compile_dynamics()
-        self.boundary_conditions = self.compile_boundary_conditions()
+        arg_lens = (1, self.num_states, self.num_parameters, self.num_constants)
+        self.arg_names = ('t', 'x', 'p', 'k')
 
-    def compile_dynamics(self):
-        return lambdify(self.sym_args, self.src_bvp.dynamics.flat(), use_jit_compile=self.use_jit_compile)
+        self.args = [ca.SX.sym(name, length) for name, length in zip(self.arg_names, arg_lens)]
+        self.iter_args = [ca.vertsplit(arg, 1) for arg in self.args[1:]]
+        self.iter_args.insert(0, self.args[0])  # Insert time separately b/c not wrapped in list
 
-    def compile_boundary_conditions(self):
-        initial_boundary_conditions = lambdify(self.sym_args, tuple(self.src_bvp.boundary_conditions.initial.flat()),
-                                               use_jit_compile=self.use_jit_compile)
-        terminal_boundary_conditions = lambdify(self.sym_args, tuple(self.src_bvp.boundary_conditions.terminal.flat()),
-                                                use_jit_compile=self.use_jit_compile)
+        self.ca_dynamics = self.wrap_dynamics()
+        self.ca_boundary_conditions = self.wrap_boundary_conditions()
 
-        return CompBoundaryConditions(initial_boundary_conditions, terminal_boundary_conditions)
+    def wrap_dynamics(self):
+        dynamics = ca_wrap('f', self.args, self.comp_bvp.dynamics, self.iter_args,
+                           self.arg_names, 'dx_dt')
+        return dynamics
+
+    # TODO refactor into AdiffBVP class? -wlevin 4/8/2022
+    def wrap_boundary_conditions(self):
+        initial_boundary_conditions = ca_wrap('Psi_0', self.args, self.comp_bvp.boundary_conditions.initial,
+                                              self.iter_args, self.arg_names)
+        terminal_boundary_conditions = ca_wrap('Psi_f', self.args, self.comp_bvp.boundary_conditions.terminal,
+                                               self.iter_args, self.arg_names)
+        return AdiffBoundaryConditions(initial_boundary_conditions, terminal_boundary_conditions)
+    
