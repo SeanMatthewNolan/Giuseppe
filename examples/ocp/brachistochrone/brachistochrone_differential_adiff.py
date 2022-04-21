ccf5e72da9b1990ef12798b9e69e82e8203db712
winstonlevin <wlevin@purdue.edu>
2022-04-21 09:16:13 -0400

    refactor AdiffDual to take in OCP instead of Dual

--- examples/ocp/brachistochrone/brachistochrone_differential_adiff.py
+++ examples/ocp/brachistochrone/brachistochrone_differential_adiff.py
@@ -6,7 +6,7 @@ import giuseppe
 from giuseppe.continuation import ContinuationHandler, SolutionSet
 from giuseppe.io import InputOCP
 from giuseppe.numeric_solvers.bvp import ScipySolveBVP
-from giuseppe.problems.dual import SymDual, SymDualOCP, AdiffDualOCP  # , DualOCPSol
+from giuseppe.problems.dual import AdiffDual, AdiffDualOCP
 from giuseppe.problems.ocp import SymOCP, AdiffOCP
 # from giuseppe.guess_generators import generate_constant_guess
 from giuseppe.utils import Timer
@@ -44,10 +44,12 @@ ocp.add_constraint('terminal', 'y - y_f')
 
 with Timer(prefix='Complilation Time:'):
     sym_ocp = SymOCP(ocp)
+    adiff_dual = AdiffDual(sym_ocp)
     adiff_ocp = AdiffOCP(sym_ocp)
-    sym_dual = SymDual(sym_ocp)
-    sym_bvp = SymDualOCP(sym_ocp, sym_dual, control_method='differential')
-    adiff_dualocp = AdiffDualOCP(sym_bvp)
+    # adiff_ocp = AdiffOCP(sym_ocp)
+    # sym_dual = SymDual(sym_ocp)
+    # sym_bvp = SymDualOCP(sym_ocp, sym_dual, control_method='differential')
+    adiff_dualocp = AdiffDualOCP(adiff_ocp, adiff_dual)
 #     num_solver = ScipySolveBVP(comp_dual_ocp)
 #
 # n = 2
--- giuseppe/problems/dual/adiff.py
+++ giuseppe/problems/dual/adiff.py
@@ -5,40 +5,40 @@ from warnings import warn
 import casadi as ca
 
 from giuseppe.utils.mixins import Picky
-from .symbolic import SymDual, SymDualOCP, SymOCP
-from .compiled import CompDual, CompDualOCP
+from .symbolic import SymOCP
 from ..components.adiff import AdiffBoundaryConditions, AdiffCost, ca_wrap
-from ..ocp.compiled import CompCost, CompOCP
-from ..dual.compiled import CompAlgControlHandler, CompDiffControlHandler
+from ..ocp.compiled import CompOCP
+from ..ocp.adiff import AdiffOCP
 
 
 class AdiffDual(Picky):
-    SUPPORTED_INPUTS: type = Union[SymDual, CompDual]  # TODO change to take in SymOCP, CompOCP
+    SUPPORTED_INPUTS: type = Union[SymOCP, CompOCP, AdiffOCP]  # TODO change to take in SymOCP, CompOCP
 
-    def __init__(self, source_dual: SUPPORTED_INPUTS):
-        Picky.__init__(self, source_dual)
+    def __init__(self, source_ocp: SUPPORTED_INPUTS):
+        Picky.__init__(self, source_ocp)
 
-        self.src_dual = deepcopy(source_dual)
-        self.src_ocp = self.src_dual.src_ocp
+        self.src_ocp = deepcopy(source_ocp)
 
-        if isinstance(self.src_dual, CompDual):
-            if self.src_dual.use_jit_compile:
-                warn('AdiffDual cannot accept JIT compiled CompDual! Recompiling CompDual without JIT...')
-                self.comp_dual: CompDual = CompDual(self.src_dual.src_dual, use_jit_compile=False)
+        if isinstance(self.src_ocp, AdiffOCP):
+            self.adiff_ocp: AdiffOCP = deepcopy(self.src_ocp)
+        elif isinstance(self.src_ocp, CompOCP):
+            if self.src_ocp.use_jit_compile:
+                warn('AdiffDual cannot accept JIT compiled CompOCP! Recompiling CompOCP without JIT...')
+                self.adiff_ocp: AdiffOCP = AdiffOCP(self.src_ocp.src_ocp)
             else:
-                self.comp_dual: CompDual = deepcopy(self.src_dual)
-        else:
-            self.comp_dual: CompDual = CompDual(self.src_dual, use_jit_compile=False)
+                self.adiff_ocp: AdiffOCP = AdiffOCP(self.src_ocp)
+        elif isinstance(self.src_ocp, SymOCP):
+            self.adiff_ocp: AdiffOCP = AdiffOCP(self.src_ocp)
 
-        self.comp_ocp: CompOCP = CompOCP(self.src_ocp, use_jit_compile=False)
+        self.num_states = self.adiff_ocp.num_states
+        self.num_controls = self.adiff_ocp.num_controls
+        self.num_parameters = self.adiff_ocp.num_parameters
+        self.num_constants = self.adiff_ocp.num_constants
 
-        self.num_states = self.comp_ocp.num_states
-        self.num_costates = self.comp_dual.num_costates
-        self.num_controls = self.comp_ocp.num_controls
-        self.num_parameters = self.comp_ocp.num_parameters
-        self.num_initial_adjoints = self.comp_dual.num_initial_adjoints
-        self.num_terminal_adjoints = self.comp_dual.num_terminal_adjoints
-        self.num_constants = self.comp_ocp.num_constants
+        # TODO implement num_costates, num_initial_adjoints, num_terminal_adjoints
+        self.num_costates = self.num_states + self.num_parameters
+        self.num_initial_adjoints = self.adiff_ocp.ca_boundary_conditions.initial.size_out(0)[0]
+        self.num_terminal_adjoints = self.adiff_ocp.ca_boundary_conditions.terminal.size_out(0)[0]
 
         arg_lens = {'initial': (1, self.num_states, self.num_costates, self.num_controls,
                                 self.num_parameters, self.num_initial_adjoints, self.num_constants),
@@ -48,143 +48,104 @@ class AdiffDual(Picky):
                                  self.num_parameters, self.num_terminal_adjoints, self.num_constants),
                     'ocp': (1, self.num_states, self.num_controls, self.num_parameters, self.num_constants)}
 
-        self.arg_names = {'initial': ('t', 'x', 'lam', 'u', 'p', 'nu_0', 'k'),
+        self.arg_names = {'initial': ('t', 'x', 'lam', 'u', 'p', '_nu_0', 'k'),
                           'dynamic': ('t', 'x', 'lam', 'u', 'p', 'k'),
-                          'terminal': ('t', 'x', 'lam', 'u', 'p', 'nu_f', 'k'),
+                          'terminal': ('t', 'x', 'lam', 'u', 'p', '_nu_f', 'k'),
                           'ocp': ('t', 'x', 'u', 'p', 'k')}
 
         self.args = {}
         self.iter_args = {}
         for key in self.arg_names:
             self.args[key] = [ca.SX.sym(name, length) for name, length in zip(self.arg_names[key], arg_lens[key])]
-            self.iter_args[key] = [ca.vertsplit(arg, 1) for arg in self.args[key]]
-            self.iter_args[key][0] = self.iter_args[key][0][0]  # TODO 't' not treated as list, but as primitive. How can this be more elegant?
+            self.iter_args[key] = [ca.vertsplit(arg, 1) for arg in self.args[key][1:]]
+            self.iter_args[key].insert(0, self.args[key][0])  # Insert time separately b/c not wrapped in list
 
+        t = self.args['dynamic'][0]
+        x = self.args['dynamic'][1]
+        p = self.args['dynamic'][4]
+        x_and_p = ca.vcat((x, p))
+        costates = self.args['dynamic'][2]
+        lam = costates[:self.num_states]
+        _nu_0 = self.args['initial'][5]
+        _nu_f = self.args['terminal'][5]
+        psi0 = self.adiff_ocp.ca_boundary_conditions.initial
+        psif = self.adiff_ocp.ca_boundary_conditions.terminal
 
-        # TODO refactor into AdiffOCP class? -wlevin 4/8/2022
-        self.ca_dynamics = self.wrap_dynamics()
-        self.ca_boundary_conditions = self.wrap_boundary_conditions()
-        self.ca_cost = self.wrap_cost()
+        self.ca_hamiltonian = ca.Function('H', self.args['dynamic'],
+                                          (self.adiff_ocp.ca_cost.path(*self.adiff_ocp.args)
+                                           + ca.dot(lam, self.adiff_ocp.ca_dynamics(*self.adiff_ocp.args)),),
+                                          self.arg_names['dynamic'], ('H',))
+        self.ca_costate_dynamics = ca.Function('lam_dot', self.args['dynamic'],
+                                               (-ca.jacobian(self.ca_hamiltonian(*self.args['dynamic']), x_and_p),),
+                                               self.arg_names['dynamic'], ('lam_dot',))
 
-        self.ca_costate_dynamics = self.wrap_costate_dynamics()
-        self.ca_adjoined_boundary_conditions = self.wrap_adjoined_boundary_conditions()
-        self.ca_augmented_cost = self.wrap_augmented_cost()
-        self.ca_hamiltonian = self.ca_augmented_cost.path
+        initial_aug_cost = ca.Function('Phi_0adj', self.args['initial'],
+                                       (self.adiff_ocp.ca_cost.initial(*self.adiff_ocp.args)
+                                        + ca.dot(_nu_0, psi0(*self.adiff_ocp.args)),),
+                                       self.arg_names['initial'], ('Phi_0adj',))
+        terminal_aug_cost = ca.Function('Phi_fadj', self.args['terminal'],
+                                        (self.adiff_ocp.ca_cost.terminal(*self.adiff_ocp.args)
+                                         + ca.dot(_nu_f, psif(*self.adiff_ocp.args)),),
+                                        self.arg_names['terminal'], ('Phi_fadj',))
+        self.ca_augmented_cost = AdiffCost(initial_aug_cost, self.ca_hamiltonian, terminal_aug_cost)
 
-    # TODO refactor into AdiffOCP class? -wlevin 4/8/2022
-    def wrap_dynamics(self):
-        dynamics = ca_wrap('f', self.args['ocp'], self.comp_ocp.dynamics, self.iter_args['ocp'],
-                           self.arg_names['ocp'], 'dx_dt')
-        return dynamics
+        # TODO jacobian returning 0 instead of adjoints!
+        adj1 = ca.jacobian(initial_aug_cost(*self.args['initial']), t) - self.ca_hamiltonian(*self.args['dynamic'])
+        adj2 = ca.jacobian(initial_aug_cost(*self.args['initial']), x_and_p).T + costates
+        adj3 = ca.jacobian(terminal_aug_cost(*self.args['terminal']), t) + self.ca_hamiltonian(*self.args['dynamic'])
+        adj4 = ca.jacobian(terminal_aug_cost(*self.args['terminal']), x_and_p).T - costates
 
-    # TODO refactor into AdiffOCP class? -wlevin 4/8/2022
-    def wrap_boundary_conditions(self):
-        initial_boundary_conditions = ca_wrap('Psi_0', self.args['ocp'], self.comp_ocp.boundary_conditions.initial,
-                                              self.iter_args['ocp'], self.arg_names['ocp'])
-        terminal_boundary_conditions = ca_wrap('Psi_f', self.args['ocp'], self.comp_ocp.boundary_conditions.terminal,
-                                               self.iter_args['ocp'], self.arg_names['ocp'])
-        return AdiffBoundaryConditions(initial_boundary_conditions, terminal_boundary_conditions)
+        initial_adj_bcs = ca.Function('Psi_0adj', self.args['initial'],
+                                      (ca.vertcat(adj1, adj2),),
+                                      self.arg_names['initial'], ('Psi_0adj',))
+        terminal_adj_bcs = ca.Function('Psi_fadj', self.args['terminal'],
+                                       (ca.vertcat(adj3, adj4),),
+                                       self.arg_names['terminal'], ('Psi_fadj',))
+        self.ca_adj_boundary_conditions = AdiffBoundaryConditions(initial_adj_bcs, terminal_adj_bcs)
 
-    # TODO refactor into AdiffOCP class? -wlevin 4/8/2022
-    def wrap_cost(self):
-        initial_cost = ca_wrap('Phi_0', self.args['ocp'], self.comp_ocp.cost.initial,
-                               self.iter_args['ocp'], self.arg_names['ocp'])
-        path_cost = ca_wrap('L', self.args['ocp'], self.comp_ocp.cost.path,
-                            self.iter_args['ocp'], self.arg_names['ocp'])
-        terminal_cost = ca_wrap('Phi_f', self.args['ocp'], self.comp_ocp.cost.terminal,
-                                self.iter_args['ocp'], self.arg_names['ocp'])
-
-        return AdiffCost(initial_cost, path_cost, terminal_cost)
-
-    def wrap_costate_dynamics(self):
-        costate_dynamics = ca_wrap('dlam_dt', self.args['dynamic'], self.comp_dual.costate_dynamics,
-                                   self.iter_args['dynamic'], self.arg_names['dynamic'])
-        return costate_dynamics
-
-    def wrap_adjoined_boundary_conditions(self):
-        initial_boundary_conditions = ca_wrap('Psi_0adj', self.args['initial'],
-                                              self.comp_dual.adjoined_boundary_conditions.initial,
-                                              self.iter_args['initial'], self.arg_names['initial'])
-        terminal_boundary_conditions = ca_wrap('Psi_fadj', self.args['terminal'],
-                                               self.comp_dual.adjoined_boundary_conditions.terminal,
-                                               self.iter_args['terminal'], self.arg_names['terminal'])
-
-        return AdiffBoundaryConditions(initial_boundary_conditions, terminal_boundary_conditions)
-
-    def wrap_augmented_cost(self):
-        initial_aug_cost = ca_wrap('Phi_0adj', self.args['initial'], self.comp_dual.augmented_cost.initial,
-                                   self.iter_args['initial'], self.arg_names['initial'])
-        hamiltonian = ca_wrap('H', self.args['dynamic'], self.comp_dual.augmented_cost.path,
-                            self.iter_args['dynamic'], self.arg_names['dynamic'])
-        terminal_aug_cost = ca_wrap('Phi_fadj', self.args['terminal'], self.comp_dual.augmented_cost.terminal,
-                                self.iter_args['terminal'], self.arg_names['terminal'])
-
-        return AdiffCost(initial_aug_cost, hamiltonian, terminal_aug_cost)
-
-
-class AdiffAlgControlHandler:
-    def __init__(self, control_handler: CompAlgControlHandler, adiff_dual: AdiffDual):
-        self.adiff_dual = adiff_dual
-        self.control_handler = control_handler
-
-        self.comp_control = self.control_handler.control
-        self.args = self.adiff_dual.args['dynamic']
-        self.iter_args = self.adiff_dual.iter_args['dynamic']
-        self.arg_names = self.adiff_dual.arg_names['dynamic']
-
-        self.ca_control = self.wrap_control()
-
-    def wrap_control(self):
-        return ca_wrap('u', self.args, self.comp_control, self.iter_args, self.arg_names)
+# TODO implement AdiffAlgControlHandler
+# class AdiffAlgControlHandler:
+#     def __init__(self, control_handler: CompAlgControlHandler, adiff_dual: AdiffDual):
+#         self.adiff_dual = adiff_dual
+#         self.control_handler = control_handler
+#
+#         self.comp_control = self.control_handler.control
+#         self.args = self.adiff_dual.args['dynamic']
+#         self.iter_args = self.adiff_dual.iter_args['dynamic']
+#         self.arg_names = self.adiff_dual.arg_names['dynamic']
+#
+#         self.ca_control = self.wrap_control()
+#
+#     def wrap_control(self):
+#         return ca_wrap('u', self.args, self.comp_control, self.iter_args, self.arg_names)
 
 
 # TODO convert CompDiffControlHandler to AdiffDiffControlHandler -wlevin 4/8/2022
 class AdiffDiffControlHandler:
-    def __init__(self, source_handler: CompDiffControlHandler, adiff_dual: AdiffDual):
-        self.src_handler = deepcopy(source_handler)
+    def __init__(self, adiff_dual: AdiffDual):
         self.adiff_dual = adiff_dual
 
-        self.comp_control_dynamics = self.src_handler.control_dynamics
-        self.comp_control_bc = self.src_handler.control_bc
         self.args = self.adiff_dual.args['dynamic']
         self.iter_args = self.adiff_dual.iter_args['dynamic']
         self.arg_names = self.adiff_dual.arg_names['dynamic']
 
-        self.ca_control_dynamics = self.wrap_control_dynamics()
-        self.ca_control_bc = self.wrap_control_bc()
+        # self.ca_control_dynamics = self.wrap_control_dynamics()
+        # self.ca_control_bc = self.wrap_control_bc()
 
-    def wrap_control_dynamics(self):
-        return ca_wrap('u_dot', self.args, self.comp_control_dynamics, self.iter_args, self.arg_names)
+    # TODO implement ca_control_dynamics
+    # def wrap_control_dynamics(self):
 
-    def wrap_control_bc(self):
-        return ca_wrap('Hu_f', self.args, self.comp_control_bc, self.iter_args, self.arg_names)
+    # TODO implement ca_bc
+    # def wrap_control_bc(self):
 
 
-class AdiffDualOCP(Picky):
-    SUPPORTED_INPUTS: type = Union[SymDualOCP, CompDualOCP]
+class AdiffDualOCP:
+    def __init__(self, adiff_ocp: AdiffOCP, adiff_dual: AdiffDual, control_method: str = 'differential'):
+        self.ocp = adiff_ocp
+        self.dual = adiff_dual
 
-    def __init__(self, source_dualocp: SUPPORTED_INPUTS):
-        Picky.__init__(self, source_dualocp)
-
-        self.src_dualocp = deepcopy(source_dualocp)
-
-        if isinstance(self.src_dualocp, CompDualOCP):
-            if self.src_dualocp.use_jit_compile:
-                warn('AdiffDualOCP cannot accept JIT compiled CompDualOCP! Recompiling CompDualOCP without JIT...')
-                self.comp_dualocp: CompDualOCP = CompDualOCP(self.src_dualocp.src_dualocp, use_jit_compile=False)
-            else:
-                self.comp_dualocp: CompDualOCP = deepcopy(self.src_dualocp)
+        if control_method.lower() == 'differential':
+            self.control_handler = AdiffDiffControlHandler(self.dual)
         else:
-            self.comp_dualocp: CompDualOCP = CompDualOCP(self.src_dualocp, use_jit_compile=False)
-
-        self.comp_ocp: CompOCP = self.comp_dualocp.comp_ocp
-        self.adiff_dual: AdiffDual = AdiffDual(self.comp_dualocp.comp_dual)
-        self.control_handler: Union[AdiffAlgControlHandler, AdiffDiffControlHandler] = self.compile_control_handler()
-
-    def compile_control_handler(self):
-        comp_control_handler = self.comp_dualocp.control_handler
-        if isinstance(comp_control_handler, CompAlgControlHandler):
-            return AdiffAlgControlHandler(comp_control_handler, self.adiff_dual)
-
-        elif isinstance(comp_control_handler, CompDiffControlHandler):
-            return AdiffDiffControlHandler(comp_control_handler, self.adiff_dual)
+            raise NotImplementedError(
+                f'\"{control_method}\" is not an implemented control method. Try \"differential\".')
