Added differential control option which numerically solves linear equation and fixed bug which prevented arbitary expression order

examples/ocp/space_shuttle/space_shuttle_footprint.py
@@ -7,40 +7,53 @@ from giuseppe.io import InputOCP
 from giuseppe.numeric_solvers.bvp import ScipySolveBVP
 from giuseppe.problems.dual import SymDual, SymDualOCP, CompDualOCP
 from giuseppe.problems.ocp import SymOCP
+from giuseppe.problems.regularization import ControlConstraintHandler
 from giuseppe.utils import Timer
+
+import giuseppe
+giuseppe.utils.complilation.JIT_COMPILE = True
+
 
 ocp = InputOCP()
 
 ocp.set_independent('t')
 
-rho = 'rho0 * exp(-h/Hscale)'
-cd = '(cd0 + cd1 * alpha + cd2 * alpha**2)'
-cl = '(cl0 + cl1 * alpha)'
-q = '0.5 * ' + rho + '* v**2'
-L = q + ' * s_ref * ' + cl
-D = q + ' * s_ref * ' + cd
+ocp.add_expression('r', 're + h')
+ocp.add_expression('g', 'mu / r**2')
+ocp.add_expression('rho', 'rho_0 * exp(-h / h_ref)')
+ocp.add_expression('dyn_pres', '1 / 2 * rho * v ** 2 ')
+ocp.add_expression('lift', 'c_l * s_ref * dyn_pres')
+ocp.add_expression('drag', 'c_d * s_ref * dyn_pres')
+ocp.add_expression('c_l', 'a_0 + a_1 * alpha_hat')
+ocp.add_expression('c_d', 'b_0 + b_1 * alpha_hat + b_2 * alpha_hat**2')
+ocp.add_expression('alpha_hat', 'alpha * 180 / pi')
 
 ocp.add_state('h', 'v * sin(gamma)')
-ocp.add_state('phi', 'v * cos(gamma) * sin(psi) / ((re + h) * cos(theta))')
-ocp.add_state('theta', 'v * cos(gamma) * cos(psi) / (re + h)')
-ocp.add_state('v', '-' + D + '/ m - mu * sin(gamma) / (re + h)**2')
-ocp.add_state('gamma', L + '/(m*v) + (v/(re + h) - mu/(v*(re + h)**2))*cos(gamma)')
-ocp.add_state('psi', L + '/(m*v) + (v/(re + h) - mu/(v*(re + h)**2))*cos(gamma)')
+ocp.add_state('phi', 'v * cos(gamma) * sin(psi) / (r * cos(theta))')
+ocp.add_state('theta', 'v * cos(gamma) * cos(psi) / r')
+ocp.add_state('v', '-drag / m - g * sin(gamma)')
+ocp.add_state('gamma', 'lift * cos(beta) / (m * v) + cos(gamma) * (v / r - g / v)')
+ocp.add_state('psi', 'lift * sin(beta)/(m * v * cos(gamma)) + v * cos(gamma) * sin(psi) * sin(theta)/(r * cos(theta))')
 
 ocp.add_control('alpha')
+ocp.add_control('beta')
 
 ocp.add_constant('rho_0', 0.002378)
-ocp.add_constant('h_r', 23_800)
-ocp.add_constant('cd0', 0.26943)
-ocp.add_constant('cd1', -0.4113)
-ocp.add_constant('cd2', 18.231)
-ocp.add_constant('cl0', 0.1758)
-ocp.add_constant('cl1', 10.305)
-ocp.add_constant('Sref', 0.35)
+ocp.add_constant('h_ref', 23_800)
 ocp.add_constant('re', 20_902_900)
 ocp.add_constant('m', 203_000 / 32.174)
-ocp.add_constant('mu', 3.986e14)
+ocp.add_constant('mu', 0.14076539e17)
 
+ocp.add_constant('a_0', -0.20704)
+ocp.add_constant('a_1', 0.029244)
+ocp.add_constant('b_0', 0.07854)
+ocp.add_constant('b_1', -0.61592e-2)
+ocp.add_constant('b_2', 0.621408e-3)
+ocp.add_constant('s_ref', 2690)
+
+ocp.add_constant('eps_alpha', 1e-8)
+ocp.add_constant('alpha_min', -40 / 180 * 3.1419)
+ocp.add_constant('alpha_max', 40 / 180 * 3.1419)
 
 ocp.add_constant('h_0', 260_000)
 ocp.add_constant('phi_0', 0)
@@ -49,41 +62,48 @@ ocp.add_constant('v_0', 25_600)
 ocp.add_constant('gamma_0', -1 / 180 * np.pi)
 ocp.add_constant('psi_0', np.pi / 2)
 
-ocp.add_constant('h_f')
-ocp.add_constant('theta_f')
+ocp.add_constant('h_f', 260_000)
+ocp.add_constant('v_f', 25_600)
+ocp.add_constant('gamma_f', -1 / 180 * np.pi)
 
-ocp.set_cost('0', '0', 'v')
+ocp.set_cost('0', '0', '-phi')
 
 ocp.add_constraint('initial', 't')
 ocp.add_constraint('initial', 'h - h_0')
+ocp.add_constraint('initial', 'phi - phi_0')
 ocp.add_constraint('initial', 'theta - theta_0')
 ocp.add_constraint('initial', 'v - v_0')
 ocp.add_constraint('initial', 'gamma - gamma_0')
+ocp.add_constraint('initial', 'psi - psi_0')
 
 ocp.add_constraint('terminal', 'h - h_f')
-ocp.add_constraint('terminal', 'theta - theta_f')
+ocp.add_constraint('terminal', 'v - v_f')
+ocp.add_constraint('terminal', 'gamma - gamma_f')
+
+ocp.add_inequality_constraint('control', 'alpha', lower_limit='alpha_min', upper_limit='alpha_max',
+                              regularizer=ControlConstraintHandler('eps_alpha', method='sin'))
 
 with Timer(prefix='Complilation Time:'):
     sym_ocp = SymOCP(ocp)
     sym_dual = SymDual(sym_ocp)
-    sym_bvp = SymDualOCP(sym_ocp, sym_dual, control_method='differential')
+    sym_bvp = SymDualOCP(sym_ocp, sym_dual, control_method='differential_numeric')
     comp_dual_ocp = CompDualOCP(sym_bvp)
     num_solver = ScipySolveBVP(comp_dual_ocp)
 
-guess = auto_propagate_guess(comp_dual_ocp, control=0, t_span=10)
+guess = auto_propagate_guess(comp_dual_ocp, control=(5/180*3.14159, 0), t_span=10)
+with open('guess.data', 'wb') as file:
+    pickle.dump(guess, file)
+
 seed_sol = num_solver.solve(guess.k, guess)
+print(seed_sol.converged)
+
+with open('seed.data', 'wb') as file:
+    pickle.dump(seed_sol, file)
+
 sol_set = SolutionSet(sym_bvp, seed_sol)
 cont = ContinuationHandler(sol_set)
-cont.add_linear_series(10, {'h_f': h_f, 'theta_f': theta_f_guess}, bisection=True)
-cont.add_linear_series(500, {'v_0': v_0, 'h_0': 10e3, 'theta_f': 15e3 / re}, bisection=True)
-# cont.add_linear_series(500, {'h_0': 20e3, 'theta_f': 40e3 / re}, bisection=True)
-# cont.add_linear_series(1000, {'v_0': v_0, 'h_0': h_0, 'theta_f': theta_f}, bisection=True)
-
-with Timer(prefix='Continuation Time:'):
-    for series in cont.continuation_series:
-        for k, last_sol in series:
-            sol_i = num_solver.solve(k, last_sol)
-            sol_set.append(sol_i)
+cont.add_linear_series(1000, {'h_f': 80000, 'v_f': 2500}, bisection=True)
+sol_set = cont.run_continuation(num_solver)
 
 with open('sol_set.data', 'wb') as file:
     pickle.dump(sol_set, file)

examples/ocp/space_shuttle/space_shuttle_plot.py
@@ -3,51 +3,113 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-with open('sol_set.data', 'rb') as file:
-    sol = pickle.load(file)[-1]
+DATA = 2
+
+if DATA == 0:
+    with open('sol_set.data', 'rb') as file:
+        sol = pickle.load(file)[-1]
+elif DATA == 1:
+    with open('seed.data', 'rb') as file:
+        sol = pickle.load(file)
+elif DATA == 2:
+    with open('guess.data', 'rb') as file:
+        sol = pickle.load(file)
 
 fig = plt.figure(figsize=(6.5, 5))
-title = fig.suptitle('Glide Vehicle')
+title = fig.suptitle('Space Shuttle Crossrange')
 
 ax1 = fig.add_subplot(4, 1, 1)
 ax1.plot(sol.x[1, :] * 180/np.pi, sol.x[0, :] / 1000)
-xlabel_1 = ax1.set_xlabel(r'$\theta$ [deg]')
+xlabel_1 = ax1.set_xlabel(r'$\phi$ [deg]')
 ylabel_1 = ax1.set_ylabel(r'$h$ [km]')
 
 ax2 = fig.add_subplot(4, 1, 2)
 ax2.plot(sol.t, sol.u[0, :] * 180 / 3.14159)
+ax2.plot(sol.t, sol.u[1, :] * 180 / 3.14159)
 xlabel_2 = ax2.set_xlabel(r'$t$ [s]')
 ylabel_2 = ax2.set_ylabel(r'$\alpha$ [deg]')
 
 ax3 = fig.add_subplot(4, 1, 3)
-ax3.plot(sol.t, sol.x[2, :])
-xlabel_3 = ax3.set_xlabel(r'$t$ [s]')
-ylabel_3 = ax3.set_ylabel(r'$v$ [m/s]')
+ax3.plot(sol.x[1, :], sol.x[2, :])
+xlabel_3 = ax3.set_xlabel(r'$\phi$ [s]')
+ylabel_3 = ax3.set_ylabel(r'$\theta$ [m/s]')
 
 ax4 = fig.add_subplot(4, 1, 4)
-ax4.plot(sol.t, sol.x[3, :] * 180 / 3.14159)
+ax4.plot(sol.t, sol.x[4, :] * 180 / 3.14159)
 xlabel_4 = ax4.set_xlabel(r'$t$ [s]')
 ylabel_4 = ax4.set_ylabel(r'$\gamma$ [deg]')
+
+fig.tight_layout()
+
+fig_states = plt.figure()
+fig_states.suptitle('States')
+
+ax21 = fig_states.add_subplot(2, 3, 1)
+ax21.plot(sol.t, sol.x[0, :])
+ax21.set_xlabel(r'$t$')
+ax21.set_ylabel(r'$h$')
+
+ax22 = fig_states.add_subplot(2, 3, 2)
+ax22.plot(sol.t, sol.x[1, :])
+ax22.set_xlabel(r'$t$')
+ax22.set_ylabel(r'$\phi$')
+
+ax23 = fig_states.add_subplot(2, 3, 3)
+ax23.plot(sol.t, sol.x[2, :])
+ax23.set_xlabel(r'$t$')
+ax23.set_ylabel(r'$\theta$')
+
+ax24 = fig_states.add_subplot(2, 3, 4)
+ax24.plot(sol.t, sol.x[3, :])
+ax24.set_xlabel(r'$t$')
+ax24.set_ylabel(r'$v$')
+
+ax24 = fig_states.add_subplot(2, 3, 5)
+ax24.plot(sol.t, sol.x[4, :])
+ax24.set_xlabel(r'$t$')
+ax24.set_ylabel(r'$\gamma$')
+
+ax24 = fig_states.add_subplot(2, 3, 6)
+ax24.plot(sol.t, sol.x[5, :])
+ax24.set_xlabel(r'$t$')
+ax24.set_ylabel(r'$\psi$')
+
+fig_states.tight_layout()
 
 fig.tight_layout()
 
 fig_lam = plt.figure()
 fig_lam.suptitle('Costates')
 
-ax21 = fig_lam.add_subplot(3, 1, 1)
+ax21 = fig_lam.add_subplot(2, 3, 1)
 ax21.plot(sol.t, sol.lam[0, :])
 ax21.set_xlabel(r'$t$')
-ax21.set_ylabel(r'$\lambda_x$')
+ax21.set_ylabel(r'$\lambda_h$')
 
-ax22 = fig_lam.add_subplot(3, 1, 2)
+ax22 = fig_lam.add_subplot(2, 3, 2)
 ax22.plot(sol.t, sol.lam[1, :])
 ax22.set_xlabel(r'$t$')
-ax22.set_ylabel(r'$\lambda_y$')
+ax22.set_ylabel(r'$\lambda_\phi$')
 
-ax23 = fig_lam.add_subplot(3, 1, 3)
+ax23 = fig_lam.add_subplot(2, 3, 3)
 ax23.plot(sol.t, sol.lam[2, :])
 ax23.set_xlabel(r'$t$')
-ax23.set_ylabel(r'$\lambda_v$')
+ax23.set_ylabel(r'$\lambda_\theta$')
+
+ax24 = fig_lam.add_subplot(2, 3, 4)
+ax24.plot(sol.t, sol.lam[3, :])
+ax24.set_xlabel(r'$t$')
+ax24.set_ylabel(r'$\lambda_v$')
+
+ax24 = fig_lam.add_subplot(2, 3, 5)
+ax24.plot(sol.t, sol.lam[4, :])
+ax24.set_xlabel(r'$t$')
+ax24.set_ylabel(r'$\lambda_\gamma$')
+
+ax24 = fig_lam.add_subplot(2, 3, 6)
+ax24.plot(sol.t, sol.lam[5, :])
+ax24.set_xlabel(r'$t$')
+ax24.set_ylabel(r'$\lambda_\psi$')
 
 fig_lam.tight_layout()
 

giuseppe/numeric_solvers/bvp/scipy.py
@@ -7,7 +7,7 @@ from scipy.integrate import solve_bvp
 
 from ...problems.bvp import CompBVP, BVPSol
 from ...problems.dual import CompDualOCP, DualOCPSol
-from ...problems.dual.compiled import CompAlgControlHandler, CompDiffControlHandler
+from ...problems.dual.compiled import CompAlgControlHandler, CompDiffControlHandler, CompDiffControlHandlerNumeric
 from ...utils.complilation import jit_compile
 from ...utils.mixins import Picky
 from ...utils.typing import NumbaArray, NumbaMatrix, NPArray
@@ -88,7 +88,7 @@ class ScipySolveBVP(Picky):
                 boundary_conditions = self._generate_ocp_alg_bcs(bvp)
                 preprocess = self._preprocess_ocp_alg_sol
                 postprocess = self._generate_postprocess_ocp_alg_sol(bvp)
-            elif type(bvp.control_handler) is CompDiffControlHandler:
+            elif type(bvp.control_handler) in [CompDiffControlHandler, CompDiffControlHandlerNumeric]:
                 dynamics = self._generate_ocp_diff_dynamics(bvp)
                 boundary_conditions = self._generate_ocp_diff_bcs(bvp)
                 preprocess = self._preprocess_ocp_diff_sol

giuseppe/problems/dual/compiled.py
@@ -7,7 +7,8 @@ from numpy.typing import ArrayLike
 from giuseppe.utils.complilation import lambdify, jit_compile
 from giuseppe.utils.mixins import Picky
 from giuseppe.utils.typing import NumbaFloat, NumbaArray, SymMatrix
-from .symbolic import SymDual, SymDualOCP, SymOCP, AlgebraicControlHandler, DifferentialControlHandler
+from .symbolic import SymDual, SymDualOCP, SymOCP, AlgebraicControlHandler, DifferentialControlHandler,\
+    DifferentialControlHandlerNumeric
 from ..components.compiled import CompBoundaryConditions, CompCost
 from ..ocp.compiled import CompOCP
 
@@ -133,6 +134,35 @@ class CompDiffControlHandler:
         return lambdify(self.sym_args, self.src_handler.h_u.flat(), use_jit_compile=self.use_jit_compile)
 
 
+class CompDiffControlHandlerNumeric:
+    def __init__(self, source_handler: DifferentialControlHandlerNumeric, comp_dual: CompDual):
+        self.src_handler = deepcopy(source_handler)
+        self.comp_dual = comp_dual
+
+        self.use_jit_compile = comp_dual.use_jit_compile
+        self.sym_args = self.comp_dual.sym_args['dynamic']
+        self.args_numba_signature = self.comp_dual.args_numba_signature['dynamic']
+
+        self.control_dynamics = self.compile_control_rate()
+        self.control_bc = self.compile_control_bc()
+
+    def compile_control_rate(self):
+        compute_h_uu = lambdify(self.sym_args, self.src_handler.h_uu, use_jit_compile=self.use_jit_compile)
+        compute_rhs = lambdify(self.sym_args, self.src_handler.rhs.flat(), use_jit_compile=self.use_jit_compile)
+
+        def control_dynamics(_t, _x, _lam, _u, _p, _k):
+            return np.linalg.solve(-compute_h_uu(_t, _x, _lam, _u, _p, _k),
+                                   np.asarray(compute_rhs(_t, _x, _lam, _u, _p, _k)))
+
+        if self.use_jit_compile:
+            control_dynamics = jit_compile(control_dynamics, self.args_numba_signature)
+
+        return control_dynamics
+
+    def compile_control_bc(self):
+        return lambdify(self.sym_args, self.src_handler.h_u.flat(), use_jit_compile=self.use_jit_compile)
+
+
 class CompDualOCP(Picky):
     SUPPORTED_INPUTS: type = Union[SymDualOCP]
 
@@ -153,3 +183,6 @@ class CompDualOCP(Picky):
 
         elif type(sym_control_handler) is DifferentialControlHandler:
             return CompDiffControlHandler(sym_control_handler, self.comp_dual)
+
+        elif type(sym_control_handler) is DifferentialControlHandlerNumeric:
+            return CompDiffControlHandlerNumeric(sym_control_handler, self.comp_dual)