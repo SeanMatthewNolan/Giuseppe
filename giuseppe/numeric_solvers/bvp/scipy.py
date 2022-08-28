d7bd75631ce85ca37c34acf990bc2aaebcf575b0
winstonlevin <wlevin@purdue.edu>
2022-08-27 21:03:20 -0400

    Refactor to return to iteration bc issues with Numba

--- giuseppe/numeric_solvers/bvp/scipy.py
+++ giuseppe/numeric_solvers/bvp/scipy.py
@@ -271,26 +271,6 @@ class ScipySolveBVP(Picky):
         costate_dyn = dual_ocp.comp_dual.costate_dynamics
         control_dyn = dual_ocp.control_handler.control_dynamics
 
-        state_dyn_map = np.vectorize(pyfunc=lambda t, x, u, p, k: np.asarray(state_dyn(t, x, u, p, k)),
-                                     signature='(1),(nx),(nu)->(nx)')
-        state_dyn_map.excluded.add(3)
-        state_dyn_map.excluded.add(4)
-        costate_dyn_map = np.vectorize(pyfunc=lambda t, x, lam, u, p, k: np.asarray(costate_dyn(t, x, lam, u, p, k)),
-                                       signature='(1),(nx),(nlam),(nu)->(nlam)')
-        costate_dyn_map.excluded.add(4)
-        costate_dyn_map.excluded.add(5)
-        control_dyn_map = np.vectorize(pyfunc=lambda t, x, lam, u, p, k: np.asarray(control_dyn(t, x, lam, u, p, k)),
-                                       signature='(1),(nx),(nlam),(nu)->(nu)')
-        control_dyn_map.excluded.add(4)
-        control_dyn_map.excluded.add(5)
-
-        y_dyn_map = np.vectorize(pyfunc=lambda t, x, lam, u, p, k: np.concatenate((state_dyn(t, x, u, p, k),
-                                                                                   costate_dyn(t, x, lam, u, p, k),
-                                                                                   control_dyn(t, x, lam, u, p, k))),
-                                 signature='(1),(nx),(nlam),(nu)->(ny)')
-        y_dyn_map.excluded.add(4)
-        y_dyn_map.excluded.add(5)
-
         n_x = dual_ocp.comp_ocp.num_states
         n_p = dual_ocp.comp_ocp.num_parameters
         n_lam = dual_ocp.comp_dual.num_costates
@@ -303,11 +283,14 @@ class ScipySolveBVP(Picky):
             t_vec = t_vec.reshape(len(t_vec), 1)
             p = pnut[:n_p]
 
-            x_vec = y_vec[:n_x, :].T
-            lam_vec = y_vec[n_x:n_x+n_lam, :].T
-            u_vec = y_vec[n_x+n_lam:n_x+n_lam+n_u, :].T
-
-            y_dot = y_dyn_map(t_vec, x_vec, lam_vec, u_vec, p, k).T
+            y_dot = np.empty_like(y_vec) # Pre-allocate for Numba
+            for idx, (ti, yi) in enumerate(zip(t_vec, y_vec.T)):
+                xi = yi[:n_x]
+                lami = yi[n_x:n_x + n_lam]
+                ui = yi[n_x + n_lam:n_x + n_lam + n_u]
+                y_dot[:n_x, idx] = state_dyn(ti, xi, ui, p, k)
+                y_dot[n_x:n_x + n_lam, idx] = costate_dyn(ti, xi, lami, ui, p, k)
+                y_dot[n_x + n_lam:n_x + n_lam + n_u, idx] = control_dyn(ti, xi, lami, ui, p, k)
 
             return y_dot * tau_mult
 
