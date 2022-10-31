02266835035f6c47f8da36db96f888ce5ae68d73
Winston C. Levin <winstonclevin@gmail.com>
2022-10-31 14:14:45 -0400

    Plotting

--- examples/ocp/space_shuttle/space_shuttle_plot_conditional.py
+++ examples/ocp/space_shuttle/space_shuttle_plot_conditional.py
@@ -12,7 +12,7 @@ from space_shuttle_footprint_conditional_atm import adiff_bvp
 SMALL_FIGSIZE = (6.5, 3)
 MED_FIG_SIZE = (6.5, 5)
 BIG_FIG_SIZE = (6.5, 6.5)
-T_LAB = 'Time [s]'
+T_LAB = r'$t$ [s]'
 
 r2d = 180 / np.pi
 d2r = np.pi / 180
@@ -27,65 +27,44 @@ with open('sol_set_conditional_25_000.data', 'rb') as file:
     sol_cond_25k = pickle.load(file)[-1]
 
 # FIGURE 1 (STATES)
-fig1 = plt.figure(figsize=BIG_FIG_SIZE)
+fig1 = plt.figure(figsize=MED_FIG_SIZE)
 
 plot_nums = (1, 3, 5, 2, 4, 6)
-titles = ('Altitude [100,000 ft]', 'Cross-Range [deg]', 'Down-Range [deg]',
-          'Velocity [1,000 ft/s]', 'Flight Path Angle [deg]', 'Heading Angle [deg]')
+ylabs = (r'$h$ [100,000 ft]', r'$\phi$ [deg]', r'$\theta$ [deg]',
+          r'$V$ [1,000 ft/s]', r'$\gamma$ [deg]', r'$\psi$ [deg]')
 mult = (1e-5, r2d, r2d, 1e-3, r2d, r2d)
 ax1 = []
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 for idx, plot_num in enumerate(plot_nums):
     ax1.append(fig1.add_subplot(3, 2, plot_num))
-    h1 = ax1[idx].plot(sol.t, sol.x[idx, :] * mult[idx], color=colors[0], label=r'Exp. ($h_f = 80,000$ ft)')
-    h2 = ax1[idx].plot(sol_cond.t, sol_cond.x[idx, :] * mult[idx], color=colors[1], label=r'Cond. ($h_f = 80,000$ ft)')
-    h3 = ax1[idx].plot(sol_25k.t, sol_25k.x[idx, :] * mult[idx], '--', color=colors[0],
-                       label=r'Exp. ($h_f = 25,000$ ft)')
-    h4 = ax1[idx].plot(sol_cond_25k.t, sol_cond_25k.x[idx, :] * mult[idx], '--', color=colors[1],
-                       label=r'Cond. ($h_f = 25,000$ ft)')
-
-    h1[0].set_zorder(1)
-    h2[0].set_zorder(0)
-    h3[0].set_zorder(3)
-    h4[0].set_zorder(2)
-
-    ax1[idx].set_xlabel(T_LAB)
-    ax1[idx].set_title(titles[idx])
     ax1[idx].grid()
+    ax1[idx].plot(sol.t, sol.x[idx, :] * mult[idx], color=colors[0], label='Exp.')
+    ax1[idx].plot(sol_cond.t, sol_cond.x[idx, :] * mult[idx], zorder=0, color=colors[1], label='Cond.')
+    ax1[idx].set_ylabel(ylabs[idx])
 
 ax1[4].legend()
+ax1[-1].set_xlabel(T_LAB)
+ax1[2].set_xlabel(T_LAB)
 
 fig1.tight_layout()
 
 # FIGURE 2 (CONTROL HISTORIES)
-fig2 = plt.figure(figsize=MED_FIG_SIZE)
+fig2 = plt.figure(figsize=SMALL_FIGSIZE)
 
-plot_nums = (1, 3, 2, 4)
-indices = (0, 1, 0, 1)
-mult = (r2d, r2d, r2d, r2d)
-line_styles = ('-', '--')
-ax2 = []
+ax21 = fig2.add_subplot(211)
+ax21.grid()
+ax21.plot(sol.t, sol.u[0, :] * r2d, label='Exp.')
+ax21.plot(sol_cond.t, sol_cond.u[0, :] * r2d, zorder=0, label='Cond.')
+ax21.set_ylabel(r'$\alpha$ [deg]')
+ax21.legend()
 
-for ax_idx, plot_num in enumerate(plot_nums):
-    ax2.append(fig2.add_subplot(2, 2, plot_num))
-    idx = indices[ax_idx]
-
-    if plot_num % 2 == 1:  # Left Column of plots
-        ax2[ax_idx].plot(sol.t, sol.u[idx, :] * mult[idx], line_styles[idx], label='Exp.')
-        ax2[ax_idx].plot(sol_cond.t, sol_cond.u[idx, :] * mult[idx], zorder=0, label='Cond.')
-        ax2[ax_idx].grid()
-    else:  # Right column of plots
-        ax2[ax_idx].plot(sol_25k.t, sol_25k.u[idx, :] * mult[idx], label='Exp.')
-        ax2[ax_idx].plot(sol_cond_25k.t, sol_cond_25k.u[idx, :] * mult[idx], zorder=0, label='Cond.')
-        ax2[ax_idx].grid()
-
-ax2[0].set_ylabel('Angle of Attack [deg]')
-ax2[0].set_title(r'$h_f = 80,000$ ft')
-ax2[1].set_ylabel('Bank Angle [deg]')
-ax2[1].set_xlabel(T_LAB)
-ax2[2].set_title(r'$h_f = 25,000$ ft')
-ax2[3].set_xlabel(T_LAB)
+ax22 = fig2.add_subplot(212)
+ax22.grid()
+ax22.plot(sol.t, sol.u[1, :] * r2d, label='Exp.')
+ax22.plot(sol_cond.t, sol_cond.u[1, :] * r2d, zorder=0, label='Cond.')
+ax22.set_ylabel(r'$\beta$ [deg]')
+ax22.set_xlabel(T_LAB)
 
 fig2.tight_layout()
 
@@ -100,8 +79,6 @@ h_ref = 23_800  # ft
 
 dens_exp = rho_0 * np.exp(-sol.x[0, :] / h_ref)
 dens_exp_deriv = -rho_0 / h_ref * np.exp(-sol.x[0, :] / h_ref)
-dens_exp_25k = rho_0 * np.exp(-sol_25k.x[0, :] / h_ref)
-dens_exp_deriv_25k = -rho_0 / h_ref * np.exp(-sol_25k.x[0, :] / h_ref)
 
 h_sx = ca.SX.sym('h', 1)
 _, __, dens_expr = atm.get_ca_atm_expr(h_sx)
@@ -112,71 +89,59 @@ dens_deriv_ca_func = ca.Function('drho_dh', (h_sx,), (dens_deriv_expr,), ('h',),
 
 dens_cond = np.empty(shape=sol_cond.x[0, :].shape)
 dens_cond_deriv = np.empty(shape=sol_cond.x[0, :].shape)
-dens_cond_25k = np.empty(shape=sol_cond_25k.x[0, :].shape)
-dens_cond_deriv_25k = np.empty(shape=sol_cond_25k.x[0, :].shape)
 layer_cond = list()
-layer_cond_25k = list()
 
 for i, h in enumerate(sol_cond.x[0, :]):
     dens_cond[i] = dens_ca_func(h)
     dens_cond_deriv[i] = dens_deriv_ca_func(h)
     layer_cond.append(atm.layer(h))
 
-for i, h in enumerate(sol_cond_25k.x[0, :]):
-    dens_cond_25k[i] = dens_ca_func(h)
-    dens_cond_deriv_25k[i] = dens_deriv_ca_func(h)
-    layer_cond_25k.append(atm.layer(h))
-
 layer_cond = np.array(layer_cond)
-layer_cond_25k = np.array(layer_cond_25k)
 
-fig3 = plt.figure(figsize=MED_FIG_SIZE)
+fig3 = plt.figure(figsize=SMALL_FIGSIZE)
 
-ax31 = fig3.add_subplot(221)
-ax32 = fig3.add_subplot(222)
-ax33 = fig3.add_subplot(223)
-ax34 = fig3.add_subplot(224)
-
-ax31.plot(sol.t, dens_exp * 100_000, label='Exponential Atm.')
-ax31.plot(sol_cond.t, dens_cond * 100_000,  zorder=0, label='Conditional Atm.')
-
-ax32.plot(sol_25k.t, dens_exp_25k * 100_000, label='Exponential')
-ax32.plot(sol_cond_25k.t, dens_cond_25k * 100_000,  zorder=0, label='Conditional Atm.')
-
-ax33.plot(sol.t, dens_exp_deriv * 1e9, label='Exponential')
-ax33.plot(sol_cond.t, dens_cond_deriv * 1e9,  zorder=0, label='Conditional Atm.')
-
-ax34.plot(sol_25k.t, dens_exp_deriv_25k * 1e9, label='Exponential')
-ax34.plot(sol_cond_25k.t, dens_cond_deriv_25k * 1e9, zorder=0, label='Conditional Atm.')
-
-# for layer in atm.layer_names:
-#     layer_idcs = np.where(layer_cond == layer)
-#     if len(layer_idcs[0]) > 0:
-#         ax31.plot(sol_cond.t[layer_idcs], dens_cond[layer_idcs] * 100_000, label=layer)
-#         ax33.plot(sol_cond.t[layer_idcs], dens_cond_deriv[layer_idcs] * 1e9, label=layer)
-#
-#     layer_idcs = np.where(layer_cond_25k == layer)
-#     if len(layer_idcs[0]) > 0:
-#         ax32.plot(sol_cond_25k.t[layer_idcs], dens_cond_25k[layer_idcs] * 100_000, label=layer)
-#         ax34.plot(sol_cond_25k.t[layer_idcs], dens_cond_deriv_25k[layer_idcs] * 100_000, label=layer)
-
+ax31 = fig3.add_subplot(211)
 ax31.grid()
-ax32.grid()
-ax33.grid()
-ax34.grid()
+ax31.plot(sol.t, dens_exp, label='Exp.')
+ax31.plot(sol_cond.t, dens_cond,  zorder=0, label='Cond.')
+ax31.set_ylabel(r'$\rho$ [slug / ft$^3$]')
+ax31.set_yscale('log')
 
-ax33.set_xlabel(T_LAB)
-ax34.set_xlabel(T_LAB)
-ax31.set_ylabel(r'$\rho$ [slug / 100,000 ft$^3$]')
-ax33.set_ylabel(r'$\dfrac{d\rho}{dh}$ [slug / 10$^9$ ft$^4$]')
-ax31.set_title(r'$h_f = 80,000$ ft')
-ax32.set_title(r'$h_f = 25,000$ ft')
+ax32 = fig3.add_subplot(212)
+ax32.grid()
+ax32.plot(sol.t, -dens_exp_deriv, label='Exp.')
+ax32.plot(sol_cond.t, -dens_cond_deriv,  zorder=0, label='Cond.')
+ax32.set_ylabel(r'$-\dfrac{d\rho}{dh}$ [slug / 10$^9$ ft$^4$]')
+ax32.set_yscale('log')
 
 ax32.legend()
 
 fig3.tight_layout()
 
-# FIGURE 4 (VALIDATION)
+# FIGURE 4 (COSTATES)
+fig4 = plt.figure(figsize=MED_FIG_SIZE)
+
+plot_nums = (1, 3, 5, 2, 4, 6)
+ylabs = (r'$\lambda_h$ [deg/ft-s]', r'$\lambda_\phi$ [s$^{-1}$]', r'$\lambda_\theta$ [s$^{-1}$]',
+         r'$\lambda_V$ [deg/ft]', r'$\lambda_\gamma$ [s$^{-1}$]', r'$\lambda_\psi$ [s$^{-1}$]')
+mult = (r2d, 1, 1, r2d, 1, 1)
+ax4 = []
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+for idx, plot_num in enumerate(plot_nums):
+    ax4.append(fig4.add_subplot(3, 2, plot_num))
+    ax4[idx].grid()
+    ax4[idx].plot(sol.t, sol.lam[idx, :] * mult[idx], color=colors[0], label='Exp.')
+    ax4[idx].plot(sol_cond.t, sol_cond.lam[idx, :] * mult[idx], zorder=0, color=colors[1], label='Cond.')
+    ax4[idx].set_ylabel(ylabs[idx])
+
+ax4[4].legend()
+ax4[-1].set_xlabel(T_LAB)
+ax4[2].set_xlabel(T_LAB)
+
+fig4.tight_layout()
+
+# FIGURE 6 (VALIDATION)
 ham_map = adiff_bvp.dual.ca_hamiltonian.map(len(sol_cond.t))
 ham_u_func = adiff_bvp.dual.ca_dH_du
 ham_uT_func = ca.Function('dH_du_T', ham_u_func.sx_in(), (ham_u_func(*ham_u_func.sx_in()).T,))
@@ -267,6 +232,10 @@ fig2.savefig('space_shuttle_control_history.eps',
              bbox_inches='tight')
 
 fig3.savefig('space_shuttle_density.eps',
+             format='eps',
+             bbox_inches='tight')
+
+fig4.savefig('space_shuttle_costates.eps',
              format='eps',
              bbox_inches='tight')
 
