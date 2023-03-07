4dcf4be7a9dc0b48ad501cfc1b551b82ce5b4bf5
Sean Matthew Nolan <Sean.Matthew.Nolan@gmail.com>
2023-03-07 10:00:09 -0600

    Removed import bug accidently added two commits ago

--- giuseppe/guess_generation/__init__.py
+++ giuseppe/guess_generation/__init__.py
@@ -4,4 +4,3 @@ from .auto import auto_guess
 from .propagate_guess import propagate_guess, propagate_bvp_guess, propagate_ocp_guess, propagate_dual_guess,\
     auto_propagate_guess, auto_propagate_bvp_guess, auto_propagate_ocp_guess, auto_propagate_dual_guess
 from .interactive import InteractiveGuessGenerator
-from .iterative_auto import iterative_auto_guess
