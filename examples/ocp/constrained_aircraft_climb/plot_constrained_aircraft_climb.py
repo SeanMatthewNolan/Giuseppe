abe7e00ee84f457de4e9875d7d68fa27f4d4b295
winstonlevin <wlevin@purdue.edu>
2023-04-21 15:38:02 -0400

    WIP add constraints from reference

--- examples/ocp/constrained_aircraft_climb/plot_constrained_aircraft_climb.py
+++ examples/ocp/constrained_aircraft_climb/plot_constrained_aircraft_climb.py
@@ -4,16 +4,18 @@ import matplotlib as mpl
 import numpy as np
 
 DATA = 2
+PLOT = 'mach_fpa'
+# PLOT = 'unconstrained'
 
 # --- DATA PROCESSING -----------------------------------------
 if DATA == 0:
-    with open('guess.data', 'rb') as f:
+    with open(PLOT + '_guess.data', 'rb') as f:
         sol = pickle.load(f)
 elif DATA == 1:
-    with open('seed_sol.data', 'rb') as f:
+    with open(PLOT + '_seed_sol.data', 'rb') as f:
         sol = pickle.load(f)
 else:
-    with open('sol_set.data', 'rb') as f:
+    with open(PLOT + '_sol_set.data', 'rb') as f:
         sols = pickle.load(f)
         sol = sols[-1]
 
@@ -49,7 +51,8 @@ for idx, state in enumerate(list(sol.x)):
     ax.set_ylabel(ylabs[idx])
     ax.plot(sol.t, state)
 
-fig_states.suptitle(rf'$J(\alpha = {constants_dict["frac_time_cost"]}) = {sol.cost}$')
+fig_states.suptitle(rf'$J(\alpha = {constants_dict["frac_time_cost"]}) = {sol.cost:.2f}$, '
+                    + rf'$m_0 - m_f = {sol.x[-1, 0] - sol.x[-1, -1]:.2f}$, $t_f = {sol.t[-1]:.2f}$')
 fig_states.tight_layout()
 
 # PLOT CONTROL
@@ -69,5 +72,23 @@ for idx, u in enumerate(control_sets):
     ax.plot(sol.t, u)
 
 fig_controls.tight_layout()
+
+# PLOT COSTATES
+ylabs = (r'$\lambda_h$', r'$\lambda_d$', r'$\lambda_V$', r'$\lambda_\gamma$', r'$\lambda_m$')
+np.dtype(float)
+fig_costates = plt.figure()
+axes_costates = []
+
+for idx, costate in enumerate(list(sol.lam)):
+    axes_costates.append(fig_costates.add_subplot(2, 3, idx + 1))
+    ax = axes_costates[-1]
+    ax.ticklabel_format(style='plain')
+
+    ax.grid()
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel(ylabs[idx])
+    ax.plot(sol.t, costate)
+
+fig_costates.tight_layout()
 
 plt.show()
